36f8fd23547a1b4634c7976b3604ca442c8ed606
Full Stack Developer <102760454+K-0817@users.noreply.github.com>
2022-07-13 14:49:57 +0200

    update

--- web/admin/react/webpack.common.js
+++ web/admin/react/webpack.common.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-
 const config = {
 	entry: './src/index.jsx',
 	output: {
